f14555366b29fff1d297262a49300a3f1f7ae8fa
KamilStarzyk <xtarzyk@tutanota.com>
2020-07-29 22:53:15 +0200

    made lists and function for represent the board

---
+++ ttt.py
@@ -0,0 +1,22 @@
+oxox = [['.','.','.'],
+        ['.','.','.'],
+        ['.','.','.'],]
+
+
+
+
+print("  1    2    3")
+def init_board():
+
+    for row in oxox:
+        # if row == 'A':
+        #     print('B')
+        # elif row == 'B':
+        #     print('C')
+        print(row)
+
+init_board()
+
+
+
+#def get_move():
